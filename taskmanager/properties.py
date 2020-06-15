8e2a07f66e2985fe5bb948ca66d046221a38ed85
brapacki-pl <33712314+brapacki-pl@users.noreply.github.com>
2020-06-15 09:12:05 +0200

    added comments to properties file

--- taskmanager/properties.py
+++ taskmanager/properties.py
@@ -1,3 +1,5 @@
+#Maximum number of processes inside the task manager. Processes tried to be added above maximum will be handled according to ADD_METHOD approach
 MAX_CAPACITY = 10
+#How to treat processes added above set max capacity. Possible values: default, priority, fifo and capacity (same as default)
 ADD_METHOD = 'priority'
 ENVIRONMENT = 'dev'
